9e3c32383d2bdc1812c8ffe15361c6d91495fa62
duyunhe <a_crux@126.com>
2019-06-25 17:40:17 +0800

    hidden

--- cap1/xor.py
+++ cap1/xor.py
@@ -15,11 +15,22 @@ def sigmoid(x):
 def calc_error(X, T):
     i = X
     W0 = np.array([[0.6, 0.3], [0.4, 0.7]])
+    # print np.dot(W0, i)
     h = sigmoid(np.dot(W0, i))
     print h
     W1 = np.array([[0.7, 0.3]])
     o = sigmoid(np.dot(W1, h))
     print o
+    e = (T - o) * (1 - o) * o
+    print 'e', e
+    learning_rate = 0.1
+    he = np.dot(e, W1)
+    print he
+    de = learning_rate * e
+    dh = de * W1
+    print dh
+    W1 += dh
+    # print sigmoid(np.dot(W1, h))
 
 
 def main():
